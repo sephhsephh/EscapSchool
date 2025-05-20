5e0fda4d9d81d81fca40597da30c69c6ebdfb8e9
unknown <Jamestenosa0@gmail.com>
2025-05-20 08:08:09 +0800

    save

--- Assets/Scripts/UIScripts/PauseScript.cs
+++ Assets/Scripts/UIScripts/PauseScript.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // Required for scene loading
 
 public class PauseScript : MonoBehaviour
 {
     public GameObject pausepanel;
     public GameObject pauseButton;
-    
-
 
     public void onPauseButtonClick()
     {
         Debug.Log("clicked pause");
         pausepanel.SetActive(true);
     }
+
     public void onResumeButtonClick()
     {
-        Debug.Log("clicked pause");
+        Debug.Log("clicked resume");
         pausepanel.SetActive(false);
+    }
+
+    public void onQuitButtonClick()
+    {
+        Debug.Log("clicked quit");
+        SceneManager.LoadScene("MainMenu"); // Replace "MainMenu" with the actual name of your main menu scene
     }
 }
